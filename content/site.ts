fbfb16704d522959ecc9fd760f3c11dfc1dfe067
Arata Ishikawa <iarata0120@gmail.com>
2026-07-05 18:21:57 -0500

    Edited some content

--- content/site.ts
+++ content/site.ts
@@ -1,15 +1,15 @@
 export const site = {
   name: "Arata Ishikawa",
   role: "Researcher", // e.g. "PhD Student, Computer Science, XYZ University"
-  affiliation: "Your Affiliation Here",
-  email: "your.email@example.com",
-  location: "City, Country",
+  affiliation: "University of California, Berkeley",
+  email: "arataishikawa@berkeley.edu",
+  location: "Berkley, USA",
   social: {
     github: "https://github.com/arataishikawa11",
-    googleScholar: "",
-    linkedin: "",
-    twitter: "",
-    orcid: "",
+    // googleScholar: "",
+    linkedin: "https://www.linkedin.com/in/arata-ishikawa/",
+    // twitter: "",
+    // orcid: "",
   },
 };
 
